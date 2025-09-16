eed60601401274cccad750e427a586a86ca0dd0e
PineMi <pineiro.c.s.mi@gmail.com>
2025-09-16 15:35:38 -0300

    feat: added a searchbar

--- pinepokedex/pinepokedex/View/PokemonList.swift
+++ pinepokedex/pinepokedex/View/PokemonList.swift
@@ -11,15 +11,32 @@ struct PokemonList: View {
     @State private var pokemons: [Pokemon] = []
     private let service = PokemonService()
     
+    @State private var searchText = ""
+    
+    var filteredPokemons: [Pokemon] {
+            if searchText.isEmpty {
+                return pokemons
+            } else {
+                return pokemons.filter {
+                    let nameMatch = $0.name.lowercased().contains(searchText.lowercased())
+                    let idContainsMatch = String($0.id).contains(searchText)
+                    let idExactMatch = String(format: "%03d", $0.id) == searchText
+                    let typeMatch = $0.types.contains { type in
+                        type.lowercased().contains(searchText.lowercased())
+                    }
+                    return nameMatch || idContainsMatch || idExactMatch || typeMatch
+                }
+            }
+    }
+    
     var body: some View {
         NavigationSplitView {
-            List {
-                 ForEach(pokemons) { pokemon in
-                     PokemonRow(pokemon: pokemon)
-                         .background(
-                             NavigationLink(destination: PokemonDetails(pokemon: pokemon)){}
-                             .opacity(0)
-                         )
+            List(filteredPokemons) { pokemon in
+                    PokemonRow(pokemon: pokemon)
+                        .background(
+                            NavigationLink(destination: PokemonDetails(pokemon: pokemon)){}
+                            .opacity(0)
+                        )
                      
                      .swipeActions(edge: .leading, allowsFullSwipe: true) {
                          Button {
@@ -33,27 +50,27 @@ struct PokemonList: View {
                      .listRowInsets(EdgeInsets())
                      .listRowSeparator(.hidden)
                      .listRowBackground(Color.clear)
-                 }
              }
 
              .listRowSpacing(8)
              .ignoresSafeArea(.all, edges: .leading)
-            .overlay {
-                if pokemons.isEmpty {
-                    ProgressView("Catching Pokémon...")
-                }
-            }
-            .listStyle(.plain)
-            .navigationTitle("Pokédex")
-            .task {
-                if pokemons.isEmpty {
-                    do {
-                        pokemons = try await service.fetchPokemonList(limit: 151)
-                    } catch {
-                        print("Error fetching Pokémon: \(error)")
-                    }
-                }
-            }
+             .overlay {
+                 if pokemons.isEmpty {
+                     ProgressView("Catching Pokémon...")
+                 }
+             }
+             .listStyle(.plain)
+             .navigationTitle("Pokédex")
+             .searchable(text: $searchText, prompt: "Search for a Pokémon, ID or Type")
+             .task {
+                 if pokemons.isEmpty {
+                     do {
+                         pokemons = try await service.fetchPokemonList(limit: 151)
+                     } catch {
+                         print("Error fetching Pokémon: \(error)")
+                     }
+                 }
+             }
         } detail: {
             Text("Select a Pokémon")
         }
